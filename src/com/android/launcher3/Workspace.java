d6e1fb896376c966f4afe6e37062212189d33623
Alex <du.alexcruz@gmail.com>
2019-02-09 15:57:32 -0600

    Lock desktop: Ensure that widgets can't be resized
    If the desktop/homescreen is locked, this means that nothing can be
    modified on it. With this commit, we now restrict the ability to
    resize widgets when the desktop is locked.
    
    Change-Id: I872bc7236be3c32b5b217ffc02c21b8e9faa7892

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -1869,8 +1869,10 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                     item.spanX = resultSpan[0];
                     item.spanY = resultSpan[1];
                     AppWidgetHostView awhv = (AppWidgetHostView) cell;
-                    AppWidgetResizeFrame.updateWidgetSizeRanges(awhv, mLauncher, resultSpan[0],
-                            resultSpan[1]);
+                    if (!Utilities.isDesktopLocked(getContext())) {
+                        AppWidgetResizeFrame.updateWidgetSizeRanges(awhv, mLauncher, resultSpan[0],
+                                resultSpan[1]);
+                    }
                 }
 
                 if (foundCell) {
@@ -1911,7 +1913,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                         if (pInfo != null && !d.accessibleDrag) {
                             onCompleteRunnable = new Runnable() {
                                 public void run() {
-                                    if (!isPageInTransition()) {
+                                    if (!isPageInTransition() && !Utilities.isDesktopLocked(getContext())) {
                                         AppWidgetResizeFrame.showForWidget(hostView, cellLayout);
                                     }
                                 }
@@ -2544,7 +2546,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
             AppWidgetHostView finalView = isWidget ?
                     ((PendingAddWidgetInfo) pendingInfo).boundWidget : null;
 
-            if (finalView != null && updateWidgetSize) {
+            if (finalView != null && updateWidgetSize && !Utilities.isDesktopLocked(getContext())) {
                 AppWidgetResizeFrame.updateWidgetSizeRanges(finalView, mLauncher, item.spanX,
                         item.spanY);
             }

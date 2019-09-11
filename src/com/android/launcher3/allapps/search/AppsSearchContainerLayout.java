fef1967a501650cc8d7bce8b5585f5d4ff518c9d
paphonb <paphonb@gmail.com>
2019-09-11 20:19:23 +0700

    Fix search bar incorrectly positioned after swiping up to home
    Change-Id: I3c29fe43ea77f5b3a842242b1e66633eac4459de

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -92,10 +92,12 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         mSearchQueryBuilder = new SpannableStringBuilder();
         Selection.setSelection(mSearchQueryBuilder, 0);
 
-        mFixedTranslationY = getTranslationY();
+        mFixedTranslationY = Math.round(getTranslationY());
         mMarginTopAdjusting = mFixedTranslationY - getPaddingTop();
 
         setHint(prefixTextWithIcon(getContext(), R.drawable.ic_allapps_search, getHint()));
+
+        setTranslationY(0);
     }
 
     private Launcher tryGetLauncher(Context context) {
@@ -146,6 +148,7 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         int expectedLeft = parent.getPaddingLeft() + (availableWidth - myWidth) / 2;
         int shift = expectedLeft - left;
         setTranslationX(shift);
+        offsetTopAndBottom((int) mFixedTranslationY);
     }
 
     @Override

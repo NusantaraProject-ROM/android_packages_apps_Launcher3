LawnchairLauncher/lawnchair

Show the default intent chooser if Google Wallpapers isn't installed
Signed-off-by: Till Kottmann <me@deletescape.ch>

src/com/android/launcher3/views/OptionsPopupView.java
@@ -20,6 +20,7 @@ import static com.android.launcher3.Utilities.EXTRA_WALLPAPER_OFFSET;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.text.TextUtils;
@@ -38,6 +39,7 @@ import com.android.launcher3.popup.ArrowPopup;
 import com.android.launcher3.shortcuts.DeepShortcutView;
 import com.android.launcher3.userevent.nano.LauncherLogProto.Action;
 import com.android.launcher3.userevent.nano.LauncherLogProto.ControlType;
+import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.widget.WidgetsFullSheet;
 
 import java.util.ArrayList;
@@ -187,7 +189,9 @@ public class OptionsPopupView extends ArrowPopup
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
         String pickerPackage = launcher.getString(R.string.wallpaper_picker_package);
-        if (!TextUtils.isEmpty(pickerPackage)) {
+        PackageManager pm = v.getContext().getPackageManager();
+        if (!TextUtils.isEmpty(pickerPackage) && PackageManagerHelper
+                .isAppEnabled(pm, pickerPackage, 0)) {
             intent.setPackage(pickerPackage);
         } else {
             // If there is no target package, use the default intent chooser animation

NusantaraProject-ROM/android_packages_apps_Launcher3

Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: I3c5ac5046851d78a701c9a1e0b374a645b16f241

src/com/android/launcher3/views/OptionsPopupView.java
@@ -22,6 +22,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.text.TextUtils;
@@ -43,6 +44,7 @@ import com.android.launcher3.popup.ArrowPopup;
 import com.android.launcher3.shortcuts.DeepShortcutView;
 import com.android.launcher3.userevent.nano.LauncherLogProto.Action;
 import com.android.launcher3.userevent.nano.LauncherLogProto.ControlType;
+import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.widget.WidgetsFullSheet;
 
 import java.util.ArrayList;
@@ -223,7 +225,9 @@ public class OptionsPopupView extends ArrowPopup
                     "com.android.customization.picker.CustomizationPickerActivity"));
         }
         String pickerPackage = launcher.getString(R.string.wallpaper_picker_package);
-        if (!TextUtils.isEmpty(pickerPackage)) {
+        PackageManager pm = v.getContext().getPackageManager();
+        if (!TextUtils.isEmpty(pickerPackage) && PackageManagerHelper
+                .isAppInstalled(pm, pickerPackage, 0)) {
             intent.setPackage(pickerPackage);
         }
         return launcher.startActivitySafely(v, intent, null, null);